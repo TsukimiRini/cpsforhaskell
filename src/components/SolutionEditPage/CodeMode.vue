7f410f42190fe3640da7edb5cefc45bfcbeaad28
Tsukimi <1027532960@qq.com>
2020-11-27 13:33:51 +0800

    code mode added to solution edit page

---
+++ src/components/SolutionEditPage/CodeMode.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="content-container">
+		<div id="codeMode"></div>
+	</div>
+</template>
+
+<script>
+import { onMounted, reactive } from "vue";
+import ace from "ace-builds";
+import "ace-builds/webpack-resolver";
+import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/mode-haskell";
+export default {
+	setup() {
+		var code_mode_editor = reactive({});
+		onMounted(() => {
+			code_mode_editor = ace.edit("codeMode", {
+				minLines: 10,
+				value: "",
+				mode: "ace/mode/haskell",
+			});
+		});
+
+		return {
+			code_mode_editor,
+		};
+	},
+};
+</script>
+
+<style scoped>
+.content-container {
+	background: #f6f6f6;
+	border-radius: 6px;
+	padding: 15px;
+	margin: 15px 0 15px 0;
+}
+#codeMode {
+	height: 100%;
+}
+</style>
--- src/components/SolutionEditPage/EditCard.vue
+++ src/components/SolutionEditPage/EditCard.vue
@@ -3,6 +3,11 @@
 		<EditHeader></EditHeader>
 		<div class="content-grid">
 			<UIMode></UIMode>
+			<div></div>
+			<CodeMode></CodeMode>
+		</div>
+		<div class="put-center">
+			<Button label="完成" icon="pi pi-check" />
 		</div>
 	</div>
 </template>
@@ -10,11 +15,13 @@
 <script>
 import EditHeader from "@/components/SolutionEditPage/EditHeader";
 import UIMode from "./UIMode.vue";
+import CodeMode from "./CodeMode.vue";
 export default {
 	setup() {
 		return {
 			EditHeader,
 			UIMode,
+			CodeMode,
 		};
 	},
 };
@@ -30,5 +37,12 @@ export default {
 .content-grid {
 	display: grid;
 	grid-template-columns: 45% 10% 45%;
+}
+
+.put-center {
+	display: flex;
+	justify-items: center;
+	justify-content: center;
+	margin-top: 20px;
 }
 </style>
