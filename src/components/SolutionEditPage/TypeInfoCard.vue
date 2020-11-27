e8389bca81d386daeea2522466a548e33a83135a
Tsukimi <1027532960@qq.com>
2020-11-27 12:40:41 +0800

    add ace editor plugin; add type info component

---
+++ src/components/SolutionEditPage/TypeInfoCard.vue
@@ -0,0 +1,76 @@
+<template>
+	<div class="type-container">
+		<div class="one-line">
+			<div class="float-left">
+				<span>类型名：</span>
+				<InputText style="width: 100px; height: 30px; margin-right: 10px"></InputText>
+				<span>说明：</span>
+			</div>
+
+			<div class="input-text-container">
+				<InputText class="input-text-one-line"></InputText>
+			</div>
+		</div>
+
+		<div>定义：</div>
+		<div :id="id" style="margin: 15px 0 15px 0"></div>
+	</div>
+</template>
+
+<script>
+import { onMounted, reactive } from "vue";
+import ace from "ace-builds";
+import "ace-builds/webpack-resolver";
+import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/mode-haskell";
+export default {
+	props: {
+		id: {
+			type: String,
+			default: "type",
+		},
+	},
+	setup(props) {
+		var type_editor = reactive({});
+		onMounted(() => {
+			type_editor = ace.edit(props.id, {
+				maxLines: 5,
+				minLines: 5,
+				value: "",
+				mode: "ace/mode/haskell",
+			});
+		});
+
+		return {
+			type_editor,
+		};
+	},
+};
+</script>
+
+<style scoped>
+.type-container {
+	background: #ffffff;
+	border: 1px solid #bbbbbb;
+	padding: 15px;
+	margin-bottom: 15px;
+}
+
+.one-line {
+	overflow: hidden;
+	margin: 10px 0 10px 0;
+	border-radius: 6px;
+}
+.float-left {
+	float: left;
+	margin-top: 3px;
+	height: 30px;
+}
+.input-text-container {
+	overflow: hidden;
+}
+.input-text-one-line {
+	width: 100%;
+	height: 30px;
+}
+</style>
--- src/components/SolutionEditPage/UIMode.vue
+++ src/components/SolutionEditPage/UIMode.vue
@@ -1,37 +1,64 @@
 <template>
 	<div class="content-container">
-		<div style="one-line">
+		<div class="one-line">
 			<div class="field-name flex-align-center">方案名：</div>
 			<div class="input-text-container">
 				<InputText class="input-text-one-line"></InputText>
 			</div>
 		</div>
-		<div style="one-line">
+		<div class="one-line">
 			<div class="field-name flex-align-center" style="float: none">摘要：</div>
 			<div class="input-text-container">
 				<Textarea :autoResize="true" rows="4" style="width: 100%"></Textarea>
 			</div>
 		</div>
-		<div style="one-line">
+		<div class="one-line">
 			<div class="field-name flex-align-center">标签：</div>
 			<div class="input-text-container">
 				<Chips v-model="tags" class="input-text-one-line" />
 			</div>
 		</div>
-		<div style="one-line">
+		<div class="one-line">
 			<div class="field-name flex-align-center" style="float: none">源码：</div>
 			<div id="sourceCode"></div>
+		</div>
+		<div class="center-icon">
+			<i class="pi pi-angle-double-down" style="font-size: 25px; cursor: pointer"></i>
+		</div>
+		<div class="one-line">
+			<div class="field-name flex-align-center" style="float: none">数据类型：</div>
+			<TypeCard id="type1"></TypeCard>
+			<TypeCard id="type2"></TypeCard>
 		</div>
 	</div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { onMounted, reactive, ref } from "vue";
+import ace from "ace-builds";
+import "ace-builds/webpack-resolver";
+import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/mode-haskell";
+
+import TypeCard from "./TypeInfoCard.vue";
 export default {
 	setup() {
 		let tags = ref(null);
+
+		var source_editor = reactive({});
+		onMounted(() => {
+			source_editor = ace.edit("sourceCode", {
+				maxLines: 10,
+				minLines: 10,
+				value: "",
+				mode: "ace/mode/haskell",
+			});
+		});
+
 		return {
 			tags,
+			source_editor,
+			TypeCard,
 		};
 	},
 };
@@ -39,13 +66,20 @@ export default {
 
 <style scoped>
 .content-container {
-	background: #e8e8e8;
+	background: #f6f6f6;
 	border-radius: 6px;
 	padding: 15px;
+	margin: 15px 0 15px 0;
 }
 
 .one-line {
 	overflow: hidden;
+	margin: 10px 0 10px 0;
+	padding: 5px;
+	border-radius: 6px;
+}
+.one-line:hover {
+	background: #dedede99;
 }
 .field-name {
 	width: 80px;
@@ -60,5 +94,10 @@ export default {
 .input-text-one-line {
 	width: 100%;
 	height: 30px;
+}
+
+.center-icon {
+	text-align: center;
+	margin: 35px 0 35px 0;
 }
 </style>
